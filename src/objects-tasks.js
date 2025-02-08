a73fcbc536daee3065a8efe84055c6c58107813b
Marina Kholodova <marinaholodova@gmail.com>
2025-02-08 19:19:28 +0300

    feat: done removePropertie task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -58,8 +58,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const results = obj;
+  keys.forEach((key) => {
+    delete results[key];
+  });
+  return results;
 }
 
 /**
